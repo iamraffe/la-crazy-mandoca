5d9390c964274b2dee9c44396e633f980e1b0c0b
Rafa Ramírez <raffe90@gmail.com>
2015-11-26 02:43:56 +0100

    Adds default image for user

--- db/migrate/20151123205221_add_image_columns_to_users.rb
+++ db/migrate/20151123205221_add_image_columns_to_users.rb
@@ -1,5 +1,5 @@
 class AddImageColumnsToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :image, :string
+    add_column :users, :image, :string, :default => 'http://localhost:3000/assets/avatar01.jpg'
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,22 +14,22 @@
 ActiveRecord::Schema.define(version: 20151123205221) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",                                          null: false
+    t.string   "encrypted_password",     default: "",                                          null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,                                           null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                                                                   null: false
+    t.datetime "updated_at",                                                                   null: false
     t.string   "provider"
     t.string   "uid"
     t.string   "username"
-    t.string   "image"
+    t.string   "image",                  default: "http://localhost:3000/assets/avatar01.jpg"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
