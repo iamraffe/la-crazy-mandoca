5982f6813ef995c958d4aa9a30da1f8444e3f45b
Rafa Ramírez <raffe90@gmail.com>
2015-12-09 00:46:08 +0100

    Adds S3 integration

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,7 +4,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted? || @user.save
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+      set_flash_message(:success, :success, :kind => "Facebook") if is_navigational_format?
     else
       session["devise.omniauth_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
@@ -17,7 +17,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     if @user.persisted? || @user.save
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => "Twitter") if is_navigational_format?
+      set_flash_message(:success, :success, :kind => "Twitter") if is_navigational_format?
+      # byebug
     else
       session["devise.omniauth_data"] = request.env["omniauth.auth"].except("extra")
       redirect_to new_user_registration_url
@@ -30,7 +31,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     if @user.persisted? || @user.save
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => "Google") if is_navigational_format?
+      set_flash_message(:success, :success, :kind => "Google") if is_navigational_format?
     else
       session["devise.omniauth_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
