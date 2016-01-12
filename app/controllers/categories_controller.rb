b6c19248c1de6d5a4a0b5c939980754a584f7993
raffe90 <raffe90@gmail.com>
2016-01-12 16:25:55 +0100

    Category filtering

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,7 @@
 class CategoriesController < ApplicationController
   def show
     @category = Category.friendly.find(params[:id])
-    # @posts = Image.where(category_id: @category.id).order(created_at: :desc) || Video.where(category_id: @category.id).order(created_at: :desc)
-    @posts = Post.joins("INNER JOIN images ON images.id = posts.postable_id").where(:images => {category_id: @category.id})
-    byebug
+    @posts = Post.all.select { |post| post.postable.category_id == @category.id }    
+    # byebug
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,3 +2,6 @@ class Post < ActiveRecord::Base
   belongs_to :postable, :polymorphic => true
   delegate :media, to: :postable
 end
+# @posts = Post.joins('INNER JOIN "images" ON "images"."id" = "posts"."postable_id"').where(:images => {category_id: 2}).joins('INNER JOIN "videos" ON "videos"."id" = "posts"."postable_id"').where(:videos => {category_id: 2})
+
+# Post.joins('LEFT JOIN "videos" ON "videos"."id" = "posts"."postable_id" AND "postable_type" = "Video"').where(:videos => {category_id: 3}).joins('INNER JOIN "images" ON "images"."id" = "posts"."postable_id" AND "postable_type" = "Image"').where(:images => {category_id: 3})
