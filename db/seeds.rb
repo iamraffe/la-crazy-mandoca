941432d2794c38932440cee5d4cc98e24c7bb801
raffe90 <raffe90@gmail.com>
2016-04-18 15:47:16 +0200

    Updates seeds to clear database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Category.destroy_all
 Category.create([{name: "Mandocas"}])
 Category.create([{name: "Sweet"}])
 Category.create([{name: "Nerd/2"}])
 Category.create([{name: "Caliente"}])
-User.create({email: "raffe90@gmail.com", admin: true})
+User.destroy_all
+Post.destroy_all
+Image.destroy_all
+Video.destroy_all
