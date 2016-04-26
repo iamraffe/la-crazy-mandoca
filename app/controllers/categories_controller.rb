a8c30a09d12bad4352322a1f7028d94350a1dc8c
Rafa Ramírez <raffe90@gmail.com>
2016-04-27 01:54:41 +0200

    Adds top posts to category show method

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,7 @@
 class CategoriesController < ApplicationController
   def show
     @category = Category.friendly.find(params[:id])
-    @posts = Post.where(:category_id => @category.id).order(created_at: 'DESC')  
-    @top_5 = Post.all.order('cached_votes_score DESC').limit(5)
+    @posts = Post.where(:category_id => @category.id).order(created_at: 'DESC')
+    @top_posts = Post.all.order('cached_votes_score DESC').limit(16)
   end
 end
